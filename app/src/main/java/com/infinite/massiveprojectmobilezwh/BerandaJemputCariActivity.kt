2a54dcbf867e4827d7ded6fd888bbc696f5f501d
chuda203 <hudakhoir203@gmail.com>
2023-11-24 22:04:55 +0700

    Tambah layout pengguna, beranda "penjemputan sampah"

---
+++ app/src/main/java/com/infinite/massiveprojectmobilezwh/BerandaJemputCariActivity.kt
@@ -0,0 +1,11 @@
+package com.infinite.massiveprojectmobilezwh
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BerandaJemputCariActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_beranda_jemput_cari)
+    }
+}
