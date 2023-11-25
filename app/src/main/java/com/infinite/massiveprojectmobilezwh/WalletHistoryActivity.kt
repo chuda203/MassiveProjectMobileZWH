c33874c10daee4cf1b90faa7c503bf2a229abdf4
chuda203 <hudakhoir203@gmail.com>
2023-11-25 20:48:24 +0700

    Penambahan semua layout pengepul

---
+++ app/src/main/java/com/infinite/massiveprojectmobilezwh/WalletHistoryActivity.kt
@@ -0,0 +1,11 @@
+package com.infinite.massiveprojectmobilezwh
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WalletHistoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_wallet_history)
+    }
+}
