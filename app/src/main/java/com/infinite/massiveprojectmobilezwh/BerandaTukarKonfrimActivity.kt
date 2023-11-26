5a78597191adef2515d1f24215462719ef3017d2
chuda203 <hudakhoir203@gmail.com>
2023-11-26 22:39:58 +0700

    Tambah layout (Splash dan Login), dan Rename layout

---
+++ app/src/main/java/com/infinite/massiveprojectmobilezwh/BerandaTukarKonfrimActivity.kt
@@ -0,0 +1,11 @@
+package com.infinite.massiveprojectmobilezwh
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BerandaTukarKonfrimActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_beranda_tukar_konfrim)
+    }
+}
