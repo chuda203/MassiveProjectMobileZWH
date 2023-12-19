cde2a13a2012ebc45385c0fe6fcf8502cc38804f
chuda203 <hudakhoir203@gmail.com>
2023-12-19 19:58:26 +0700

    tambahin sample maps di fragment_beranda_lokasi.xml

--- app/src/main/java/com/infinite/massiveprojectmobilezwh/beranda/BerandaLokasiFragment.kt
+++ app/src/main/java/com/infinite/massiveprojectmobilezwh/beranda/BerandaLokasiFragment.kt
@@ -1,78 +1,217 @@
 package com.infinite.massiveprojectmobilezwh.beranda
+//
+//import android.content.Intent
+//import android.os.Bundle
+//import androidx.fragment.app.Fragment
+//import android.view.LayoutInflater
+//import android.view.View
+//import android.view.ViewGroup
+//import android.widget.Button
+//import android.widget.ImageView
+//import androidx.cardview.widget.CardView
+//import com.infinite.massiveprojectmobilezwh.R
+//import com.infinite.massiveprojectmobilezwh.dompet.BerandaDompetActivity
+//import com.infinite.massiveprojectmobilezwh.dompet.BerandaTukarActivity
+//
+//// TODO: Rename parameter arguments, choose names that match
+//// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+//private const val ARG_PARAM1 = "param1"
+//private const val ARG_PARAM2 = "param2"
+//
+///**
+// * A simple [Fragment] subclass.
+// * Use the [BerandaLokasiFragment.newInstance] factory method to
+// * create an instance of this fragment.
+// */
+//class BerandaLokasiFragment : Fragment() {
+//    // TODO: Rename and change types of parameters
+//    private var param1: String? = null
+//    private var param2: String? = null
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        arguments?.let {
+//            param1 = it.getString(ARG_PARAM1)
+//            param2 = it.getString(ARG_PARAM2)
+//        }
+//    }
+//
+//    override fun onCreateView(
+//        inflater: LayoutInflater, container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View? {
+//        // Inflate the layout for this fragment
+//        return inflater.inflate(R.layout.fragment_beranda_lokasi, container, false)
+//    }
+//
+//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//        super.onViewCreated(view, savedInstanceState)
+//        val btnConfrim : Button = view.findViewById(R.id.bt_confirmation)
+//        // Intent ke Capture Sampah
+//        btnConfrim.setOnClickListener {
+//            Intent(requireActivity(), BerandaCameraActivity::class.java).also {
+//                startActivity(it)
+//            }
+//        }
+//
+//    }
+//
+//    companion object {
+//        /**
+//         * Use this factory method to create a new instance of
+//         * this fragment using the provided parameters.
+//         *
+//         * @param param1 Parameter 1.
+//         * @param param2 Parameter 2.
+//         * @return A new instance of fragment BerandaLokasiFragment.
+//         */
+//        // TODO: Rename and change types and number of parameters
+//        @JvmStatic
+//        fun newInstance(param1: String, param2: String) =
+//            BerandaLokasiFragment().apply {
+//                arguments = Bundle().apply {
+//                    putString(ARG_PARAM1, param1)
+//                    putString(ARG_PARAM2, param2)
+//                }
+//            }
+//    }
+//}
 
-import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.preference.PreferenceManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.ImageView
-import androidx.cardview.widget.CardView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.infinite.massiveprojectmobilezwh.ModelMain
 import com.infinite.massiveprojectmobilezwh.R
-import com.infinite.massiveprojectmobilezwh.dompet.BerandaDompetActivity
-import com.infinite.massiveprojectmobilezwh.dompet.BerandaTukarActivity
+//import kotlinx.android.synthetic.main.fragment_map.*
+import org.json.JSONException
+import org.json.JSONObject
+import org.osmdroid.config.Configuration
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.CustomZoomButtonsController
+import org.osmdroid.views.MapController
+import org.osmdroid.views.MapView
+import org.osmdroid.views.overlay.Marker
+import org.osmdroid.views.overlay.OverlayItem
+import java.io.IOException
+import java.nio.charset.StandardCharsets
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [BerandaLokasiFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class BerandaLokasiFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    var modelMainList: MutableList<ModelMain> = ArrayList()
+    lateinit var mapController: MapController
+    lateinit var overlayItem: ArrayList<OverlayItem>
+    lateinit var mapView: MapView
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_beranda_lokasi, container, false)
+        val view = inflater.inflate(R.layout.fragment_beranda_lokasi, container, false)
+
+        Configuration.getInstance()
+            .load(requireContext(), PreferenceManager.getDefaultSharedPreferences(requireContext()))
+
+        val geoPoint = GeoPoint(-6.3035467, 106.8693513)
+        mapView = view.findViewById(R.id.mapView)
+        mapView.setMultiTouchControls(true)
+        mapView.controller.animateTo(geoPoint)
+        mapView.setTileSource(TileSourceFactory.DEFAULT_TILE_SOURCE)
+        mapView.zoomController.setVisibility(CustomZoomButtonsController.Visibility.NEVER)
+
+        mapController = mapView.controller as MapController
+        mapController.setCenter(geoPoint)
+        mapController.zoomTo(15)
+
+        getLocationMarker()
+
+        return view
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        val btnConfrim : Button = view.findViewById(R.id.bt_confirmation)
-        // Intent ke Capture Sampah
-        btnConfrim.setOnClickListener {
-            Intent(requireActivity(), BerandaCameraActivity::class.java).also {
-                startActivity(it)
-            }
-        }
+    private fun getLocationMarker() {
+        try {
+            val stream = requireActivity().assets.open("sample_maps.json")
+            val size = stream.available()
+            val buffer = ByteArray(size)
+            stream.read(buffer)
+            stream.close()
+            val strContent = String(buffer, StandardCharsets.UTF_8)
+            try {
+                val jsonObject = JSONObject(strContent)
+                val jsonArrayResult = jsonObject.getJSONArray("results")
+                for (i in 0 until jsonArrayResult.length()) {
+                    val jsonObjectResult = jsonArrayResult.getJSONObject(i)
+                    val modelMain = ModelMain()
+                    modelMain.strName = jsonObjectResult.getString("name")
+                    modelMain.strVicinity = jsonObjectResult.getString("vicinity")
 
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment BerandaLokasiFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            BerandaLokasiFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+                    // get lat long
+                    val jsonObjectGeo = jsonObjectResult.getJSONObject("geometry")
+                    val jsonObjectLoc = jsonObjectGeo.getJSONObject("location")
+                    modelMain.latLoc = jsonObjectLoc.getDouble("lat")
+                    modelMain.longLoc = jsonObjectLoc.getDouble("lng")
+                    modelMainList.add(modelMain)
                 }
+                initMarker(modelMainList)
+            } catch (e: JSONException) {
+                e.printStackTrace()
             }
+        } catch (ignored: IOException) {
+            Toast.makeText(
+                requireContext(),
+                "Oops, ada yang tidak beres. Coba ulangi beberapa saat lagi.",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
+    private fun initMarker(modelList: List<ModelMain>) {
+        for (i in modelList.indices) {
+            overlayItem = ArrayList()
+            overlayItem.add(
+                OverlayItem(
+                    modelList[i].strName, modelList[i].strVicinity, GeoPoint(
+                        modelList[i].latLoc, modelList[i].longLoc
+                    )
+                )
+            )
+            val info = ModelMain()
+            info.strName = modelList[i].strName
+            info.strVicinity = modelList[i].strVicinity
+
+            val marker = Marker(mapView)
+//            marker.icon = resources.getDrawable(R.drawable.ic_place)
+            marker.position = GeoPoint(modelList[i].latLoc, modelList[i].longLoc)
+            marker.relatedObject = info
+//            marker.infoWindow = CustomInfoWindow(mapView)
+            marker.setOnMarkerClickListener { item, arg1 ->
+                item.showInfoWindow()
+                true
+            }
+
+            mapView.overlays.add(marker)
+            mapView.invalidate()
+        }    }
+
+    public override fun onResume() {
+        super.onResume()
+        Configuration.getInstance()
+            .load(requireContext(), PreferenceManager.getDefaultSharedPreferences(requireContext()))
+        if (mapView != null) {
+            mapView.onResume()
+        }
+    }
+
+    public override fun onPause() {
+        super.onPause()
+        Configuration.getInstance()
+            .load(requireContext(), PreferenceManager.getDefaultSharedPreferences(requireContext()))
+        if (mapView != null) {
+            mapView.onPause()
+        }
     }
 }
